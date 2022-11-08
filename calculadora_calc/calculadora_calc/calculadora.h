09c379dc8a6f1527ce3a4614c7c476c4fc8c5561
JohnyChiroy <jchiroyg1@miumg.edu.gt>
2022-11-08 00:07:16 -0600

    Diseño calculadora

--- calculadora_calc/calculadora_calc/calculadora.h
+++ calculadora_calc/calculadora_calc/calculadora.h
@@ -35,9 +35,26 @@ namespace calculadoracalc {
 			}
 		}
 	private: System::Windows::Forms::PictureBox^ pictureBox2;
-	private: System::Windows::Forms::PictureBox^ pictureBox1;
-	private: System::Windows::Forms::PictureBox^ pictureBox3;
-	private: System::Windows::Forms::PictureBox^ pictureBox4;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button5;
+	private: System::Windows::Forms::Button^ button4;
+	private: System::Windows::Forms::Button^ button6;
+	private: System::Windows::Forms::Button^ button7;
+	private: System::Windows::Forms::Button^ button8;
+	private: System::Windows::Forms::Button^ button9;
+	private: System::Windows::Forms::Button^ button10;
+	private: System::Windows::Forms::Button^ button11;
+	private: System::Windows::Forms::Button^ button12;
+	private: System::Windows::Forms::Button^ button13;
+	private: System::Windows::Forms::Button^ button14;
+	private: System::Windows::Forms::Button^ button15;
+	private: System::Windows::Forms::Button^ button16;
+
+
+
 	protected:
 
 	private:
@@ -55,13 +72,24 @@ namespace calculadoracalc {
 		{
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(calculadora::typeid));
 			this->pictureBox2 = (gcnew System::Windows::Forms::PictureBox());
-			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
-			this->pictureBox3 = (gcnew System::Windows::Forms::PictureBox());
-			this->pictureBox4 = (gcnew System::Windows::Forms::PictureBox());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button5 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->button6 = (gcnew System::Windows::Forms::Button());
+			this->button7 = (gcnew System::Windows::Forms::Button());
+			this->button8 = (gcnew System::Windows::Forms::Button());
+			this->button9 = (gcnew System::Windows::Forms::Button());
+			this->button10 = (gcnew System::Windows::Forms::Button());
+			this->button11 = (gcnew System::Windows::Forms::Button());
+			this->button12 = (gcnew System::Windows::Forms::Button());
+			this->button13 = (gcnew System::Windows::Forms::Button());
+			this->button14 = (gcnew System::Windows::Forms::Button());
+			this->button15 = (gcnew System::Windows::Forms::Button());
+			this->button16 = (gcnew System::Windows::Forms::Button());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->BeginInit();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox3))->BeginInit();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox4))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// pictureBox2
@@ -70,68 +98,201 @@ namespace calculadoracalc {
 			this->pictureBox2->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox2.BackgroundImage")));
 			this->pictureBox2->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Zoom;
 			this->pictureBox2->ImageLocation = L"";
-			this->pictureBox2->Location = System::Drawing::Point(967, 13);
-			this->pictureBox2->Margin = System::Windows::Forms::Padding(4);
+			this->pictureBox2->Location = System::Drawing::Point(1289, 16);
+			this->pictureBox2->Margin = System::Windows::Forms::Padding(5, 5, 5, 5);
 			this->pictureBox2->Name = L"pictureBox2";
-			this->pictureBox2->Size = System::Drawing::Size(67, 63);
+			this->pictureBox2->Size = System::Drawing::Size(89, 78);
 			this->pictureBox2->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
 			this->pictureBox2->TabIndex = 24;
 			this->pictureBox2->TabStop = false;
 			this->pictureBox2->Click += gcnew System::EventHandler(this, &calculadora::pictureBox2_Click);
 			// 
-			// pictureBox1
+			// textBox1
 			// 
-			this->pictureBox1->BackColor = System::Drawing::Color::Transparent;
-			this->pictureBox1->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.BackgroundImage")));
-			this->pictureBox1->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Zoom;
-			this->pictureBox1->Location = System::Drawing::Point(149, 86);
-			this->pictureBox1->Name = L"pictureBox1";
-			this->pictureBox1->Size = System::Drawing::Size(140, 87);
-			this->pictureBox1->TabIndex = 25;
-			this->pictureBox1->TabStop = false;
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 19.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->textBox1->Location = System::Drawing::Point(144, 53);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(459, 45);
+			this->textBox1->TabIndex = 25;
 			// 
-			// pictureBox3
+			// button1
 			// 
-			this->pictureBox3->BackColor = System::Drawing::Color::Transparent;
-			this->pictureBox3->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox3.BackgroundImage")));
-			this->pictureBox3->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Zoom;
-			this->pictureBox3->Location = System::Drawing::Point(312, 86);
-			this->pictureBox3->Name = L"pictureBox3";
-			this->pictureBox3->Size = System::Drawing::Size(140, 87);
-			this->pictureBox3->TabIndex = 26;
-			this->pictureBox3->TabStop = false;
+			this->button1->Location = System::Drawing::Point(161, 131);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(85, 72);
+			this->button1->TabIndex = 26;
+			this->button1->Text = L"button1";
+			this->button1->UseVisualStyleBackColor = true;
 			// 
-			// pictureBox4
+			// button3
 			// 
-			this->pictureBox4->BackColor = System::Drawing::Color::Transparent;
-			this->pictureBox4->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox4.BackgroundImage")));
-			this->pictureBox4->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Zoom;
-			this->pictureBox4->Location = System::Drawing::Point(149, 179);
-			this->pictureBox4->Name = L"pictureBox4";
-			this->pictureBox4->Size = System::Drawing::Size(140, 87);
-			this->pictureBox4->TabIndex = 27;
-			this->pictureBox4->TabStop = false;
+			this->button3->Location = System::Drawing::Point(274, 131);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(85, 72);
+			this->button3->TabIndex = 27;
+			this->button3->Text = L"button2";
+			this->button3->UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(396, 131);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(85, 72);
+			this->button2->TabIndex = 28;
+			this->button2->Text = L"button2";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// button5
+			// 
+			this->button5->Location = System::Drawing::Point(504, 131);
+			this->button5->Name = L"button5";
+			this->button5->Size = System::Drawing::Size(85, 72);
+			this->button5->TabIndex = 29;
+			this->button5->Text = L"button2";
+			this->button5->UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this->button4->Location = System::Drawing::Point(504, 238);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(85, 72);
+			this->button4->TabIndex = 33;
+			this->button4->Text = L"button2";
+			this->button4->UseVisualStyleBackColor = true;
+			// 
+			// button6
+			// 
+			this->button6->Location = System::Drawing::Point(396, 238);
+			this->button6->Name = L"button6";
+			this->button6->Size = System::Drawing::Size(85, 72);
+			this->button6->TabIndex = 32;
+			this->button6->Text = L"button6";
+			this->button6->UseVisualStyleBackColor = true;
+			// 
+			// button7
+			// 
+			this->button7->Location = System::Drawing::Point(274, 238);
+			this->button7->Name = L"button7";
+			this->button7->Size = System::Drawing::Size(85, 72);
+			this->button7->TabIndex = 31;
+			this->button7->Text = L"button2";
+			this->button7->UseVisualStyleBackColor = true;
+			// 
+			// button8
+			// 
+			this->button8->Location = System::Drawing::Point(161, 238);
+			this->button8->Name = L"button8";
+			this->button8->Size = System::Drawing::Size(85, 72);
+			this->button8->TabIndex = 30;
+			this->button8->Text = L"button8";
+			this->button8->UseVisualStyleBackColor = true;
+			// 
+			// button9
+			// 
+			this->button9->Location = System::Drawing::Point(504, 338);
+			this->button9->Name = L"button9";
+			this->button9->Size = System::Drawing::Size(85, 72);
+			this->button9->TabIndex = 37;
+			this->button9->Text = L"button2";
+			this->button9->UseVisualStyleBackColor = true;
+			// 
+			// button10
+			// 
+			this->button10->Location = System::Drawing::Point(396, 338);
+			this->button10->Name = L"button10";
+			this->button10->Size = System::Drawing::Size(85, 72);
+			this->button10->TabIndex = 36;
+			this->button10->Text = L"button10";
+			this->button10->UseVisualStyleBackColor = true;
+			// 
+			// button11
+			// 
+			this->button11->Location = System::Drawing::Point(274, 338);
+			this->button11->Name = L"button11";
+			this->button11->Size = System::Drawing::Size(85, 72);
+			this->button11->TabIndex = 35;
+			this->button11->Text = L"button2";
+			this->button11->UseVisualStyleBackColor = true;
+			// 
+			// button12
+			// 
+			this->button12->Location = System::Drawing::Point(161, 338);
+			this->button12->Name = L"button12";
+			this->button12->Size = System::Drawing::Size(85, 72);
+			this->button12->TabIndex = 34;
+			this->button12->Text = L"button12";
+			this->button12->UseVisualStyleBackColor = true;
+			// 
+			// button13
+			// 
+			this->button13->Location = System::Drawing::Point(504, 433);
+			this->button13->Name = L"button13";
+			this->button13->Size = System::Drawing::Size(85, 72);
+			this->button13->TabIndex = 41;
+			this->button13->Text = L"button2";
+			this->button13->UseVisualStyleBackColor = true;
+			// 
+			// button14
+			// 
+			this->button14->Location = System::Drawing::Point(396, 433);
+			this->button14->Name = L"button14";
+			this->button14->Size = System::Drawing::Size(85, 72);
+			this->button14->TabIndex = 40;
+			this->button14->Text = L"button14";
+			this->button14->UseVisualStyleBackColor = true;
+			// 
+			// button15
+			// 
+			this->button15->Location = System::Drawing::Point(274, 433);
+			this->button15->Name = L"button15";
+			this->button15->Size = System::Drawing::Size(85, 72);
+			this->button15->TabIndex = 39;
+			this->button15->Text = L"button2";
+			this->button15->UseVisualStyleBackColor = true;
+			// 
+			// button16
+			// 
+			this->button16->Location = System::Drawing::Point(161, 433);
+			this->button16->Name = L"button16";
+			this->button16->Size = System::Drawing::Size(85, 72);
+			this->button16->TabIndex = 38;
+			this->button16->Text = L"button16";
+			this->button16->UseVisualStyleBackColor = true;
 			// 
 			// calculadora
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
 			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
-			this->ClientSize = System::Drawing::Size(1047, 584);
-			this->Controls->Add(this->pictureBox4);
-			this->Controls->Add(this->pictureBox3);
-			this->Controls->Add(this->pictureBox1);
+			this->ClientSize = System::Drawing::Size(1396, 719);
+			this->Controls->Add(this->button13);
+			this->Controls->Add(this->button14);
+			this->Controls->Add(this->button15);
+			this->Controls->Add(this->button16);
+			this->Controls->Add(this->button9);
+			this->Controls->Add(this->button10);
+			this->Controls->Add(this->button11);
+			this->Controls->Add(this->button12);
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->button6);
+			this->Controls->Add(this->button7);
+			this->Controls->Add(this->button8);
+			this->Controls->Add(this->button5);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->textBox1);
 			this->Controls->Add(this->pictureBox2);
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
+			this->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->Name = L"calculadora";
 			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 			this->Text = L"calculadora";
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->EndInit();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox3))->EndInit();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox4))->EndInit();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
